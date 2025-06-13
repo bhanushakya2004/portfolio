398f930d0de6f903bc2ad3a97dc8d355cc128cfe
Bhanu Shakya <166703471+bhanushakya2004@users.noreply.github.com>
2025-06-13 08:46:25 +0530

    Update ExperienceList.tsx

--- src/components/experience/ExperienceList.tsx
+++ src/components/experience/ExperienceList.tsx
@@ -4,14 +4,14 @@ export const ExperienceList = () => {
   return (
     <div className="flex flex-col gap-y-6">
         <ExperienceCard
-            duration="May 2025 - July 2025"
-            companyName="Clover IT Services"
-            role="Full Stack Developer"
-            companyWebsiteLink="https://www.cloveritservices.com/"
-            techStack={["Mern","Redux-toolkit","Typescript",]}
-            desc={<p>I enhanced system performance by ,<strong>reducing API response times by up to 35% through REST API integration </strong>. Collaborated with a cross-functional team to improve code quality and streamline development.</p>}
-            jobType="Internship"
-        />
+        duration="May 2025 - July 2025"
+        companyName="Innoflexion Technologies"
+        role="GenAI Intern"
+        companyWebsiteLink="https://www.innoflexion.com/"
+        techStack={["LangChain", "Gemini", "Python", "Streamlit", "Vector DBs"]}
+        desc={<p>Built <strong>AI-first agents</strong> using LangChain and Gemini to simulate personalized tutor experiences. Integrated memory and feedback systems to deliver <strong>context-aware, goal-driven tutoring agents</strong>.</p>}
+        jobType="Internship"
+      />
     </div>
   )
 }
